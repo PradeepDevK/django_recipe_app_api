f87fda96a0f2ca98dd38140d9b0181b415e75011
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-05-03 21:52:10 +0530

    checks for wait for db

---
+++ app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """Add x and y return results."""
+    return x + y
+
+def sub(x, y):
+    """Subtract x from y and return results."""
+    return y - x 
